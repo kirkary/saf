fe33f2b5aa140d91b43073774ef8a80226bc9827
kirkary <kirkary@ukr.net>
2016-01-16 14:05:56 +0200

    mobile :: screen res hotfix2

--- Go.js
+++ Go.js
@@ -4,7 +4,7 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var container =document.getElementsByClassName('container');
 
-if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     container[0].style.width = "100%";
     container[0].style.height = "100%";
 }
