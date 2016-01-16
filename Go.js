335b9656413747b492541a31073a1a5761ffa3eb
kirkary <kirkary@ukr.net>
2016-01-16 13:59:00 +0200

    mobile screen detection

--- Go.js
+++ Go.js
@@ -4,8 +4,15 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var container =document.getElementsByClassName('container');
 
-canvas.width = container[0].offsetWidth;
-canvas.height = container[0].offsetHeight;
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    container.style.width = "100%";
+    container.style.height = "100%";
+}
+else {
+    canvas.width = container[0].offsetWidth;
+    canvas.height = container[0].offsetHeight;
+}
+
 
 game.init(canvas);
 game.start();
