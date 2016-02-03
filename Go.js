6a96e90ab329b1b3e30088261c25014a28f3bad5
kirkary <kirkary@ukr.net>
2016-02-03 16:54:31 +0200

    gameplay screen :: hotFix

--- Go.js
+++ Go.js
@@ -4,10 +4,10 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var container =document.getElementsByClassName('container');
 
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    container[0].style.width = "100%";
-    container[0].style.height = "100%";
-}
+//if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+//    container[0].style.width = "100%";
+//    container[0].style.height = "100%";
+//}
 
 canvas.width = container[0].offsetWidth;
 canvas.height = container[0].offsetHeight;
