fb5ad817a93bd7cdd157d28261fcbb5f71878771
kirkary <kirkary@ukr.net>
2016-02-08 16:50:57 +0200

    screen hotfix

--- Go.js
+++ Go.js
@@ -4,15 +4,15 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var mainWrapper = document.getElementById('main-wrapper');
 var container =document.getElementsByClassName('container');
-
+var mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|iemobile|Windows Phone/i.test(navigator.userAgent);
 var resize = function(){
-if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|iemobile|Windows Phone/i.test(navigator.userAgent) ) {
+if(!mobile) {
+    container[0].style.width = "100%";
+    container[0].style.height = "100%";
     container[0].style.maxWidth = "597px";
     container[0].style.maxHeight = "768px";
 }
 else{
-    container[0].style.maxWidth = "100%";
-    container[0].style.maxHeight = "100%";
     container[0].style.width = "100%";
     container[0].style.height = "100%";
 }
@@ -27,19 +27,41 @@ if(container[0].offsetHeight > container[0].offsetWidth)
     container[0].style.height = "100%";
     canvas.height = container[0].offsetHeight;
     canvas.width =  canvas.height/ratio;
-    scale = canvas.height / contentHeight;
-    container[0].style.width = canvas.width +'px';
+    if(canvas.width > container[0].offsetWidth)
+    {
+        ratio = contentWidth/contentHeight;
+        container[0].style.width = "100%";
+        canvas.width = container[0].offsetWidth;
+        canvas.height =  canvas.width/ratio;
+        scale = canvas.width / contentWidth;
+        container[0].style.height = canvas.height +'px';
+    }
+    else{
+        scale = canvas.height / contentHeight;
+        container[0].style.width = canvas.width +'px';
+    }
 }
 else{
     ratio = contentWidth/contentHeight;
     container[0].style.width = "100%";
     canvas.width = container[0].offsetWidth;
     canvas.height =  canvas.width/ratio;
-    scale = canvas.width / contentWidth;
-    container[0].style.height = canvas.height +'px';
+    if(canvas.height > container[0].offsetHeight)
+    {
+        ratio = contentHeight/contentWidth;
+        container[0].style.height = "100%";
+        canvas.height = container[0].offsetHeight;
+        canvas.width =  canvas.height/ratio;
+        scale = canvas.height / contentHeight;
+        container[0].style.width = canvas.width +'px';
+    }
+    else{
+        scale = canvas.width / contentWidth;
+        container[0].style.height = canvas.height +'px';
+    }
 }
-
-container[0].style.top = (mainWrapper.offsetHeight - container[0].offsetHeight)/2 + 'px';
+    container[0].style.left = (mainWrapper.offsetWidth - container[0].offsetWidth)/2 + 'px';
+    container[0].style.top = (mainWrapper.offsetHeight - container[0].offsetHeight)/2 + 'px';
 return scale;
 };
 
